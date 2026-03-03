40baa1c0de8b8d9f76ab463d66a7d05e71457bde
Emilpholmedal <emilholmedal1@gmail.com>
2026-03-03 10:10:30 +0100

    firebase auth

---
+++ frontend/src/auth/authActions.js
@@ -0,0 +1,18 @@
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  signOut,
+} from "firebase/auth";
+import { auth } from "../firebase";
+
+export function registerWithEmail(email, password) {
+  return createUserWithEmailAndPassword(auth, email, password);
+}
+
+export function loginWithEmail(email, password) {
+  return signInWithEmailAndPassword(auth, email, password);
+}
+
+export function logout() {
+  return signOut(auth);
+}
