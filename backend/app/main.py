33430834298085f61eff8efd851fc896774badd3
Loland30 <Goran.Loland30@outlook.com>
2026-03-11 11:14:02 +0100

    Fixed prefix cringe

--- backend/app/main.py
+++ backend/app/main.py
@@ -18,8 +18,8 @@ app.add_middleware(
 
 # --- Routers --- #
 app.include_router(ivf.router, prefix="/ivf")
-app.include_router(calculation.router, prefix="/calculation")
-app.include_router(terrain.router, prefix="/terrain")
+app.include_router(calculation.router)
+app.include_router(terrain.router)
 
 @app.post("/generate-pdf/{project_name}")
 def generate_pdf(project_name: str):
